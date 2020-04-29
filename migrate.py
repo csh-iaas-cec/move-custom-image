fea8370f23d49245ac22afc30f720fa9d3057572
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2020-04-29 12:34:57 +0530

    updated indent error

--- migrate.py
+++ migrate.py
@@ -6,7 +6,7 @@ from config import Config
 from oci.config import validate_config
 import oci
 import concurrent.futures
-
+import argparse
 
 logger = logs.logger
 REGIONS_SHORT_NAMES = {
@@ -216,29 +216,29 @@ class Migrate:
 
 if __name__ == "__main__":
 	description = "\n".join(["Migrates the custom images to given destination regions","pip install -r requirements.txt","python migrate.py <images_list_file_name.txt> iad lhr bom phx"])
-    parser = argparse.ArgumentParser(description=description,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--profile', help='Provide the profile to be used', required=True)
-    parser.add_argument('--file',
-                        help="Provide the text file which contains image ocids. ", required=True)
-    parser.add_argument('--regions', nargs='+',
-                        help="Updates the volume and backups tags from the given instance id", required=True)
-    parser.add_argument('--compartment_id', help='Provide the compartment ID where the images exist. (OPTIONAL) If not provided takes default compartment id')
-    parser.add_argument('--bucket_name', help="Provide bucket name for the images to be stored. OPTIONAL if not provided takes default bucket")
-    
-    
-    args = parser.parse_args()
-    PROFILE = args.profile
-    compartment_id = None
-    bucket_name = None
-    if(args.compartment_id):
-        compartment_id = args.compartment_id
-    if(args.bucket_name):
-        bucket_name = args.bucket_name
-    image_file = args.file
-    regions_list = args.regions
+	parser = argparse.ArgumentParser(description=description,
+									 formatter_class=argparse.RawDescriptionHelpFormatter)
+	parser.add_argument('--profile', help='Provide the profile to be used', required=True)
+	parser.add_argument('--file',
+						help="Provide the text file which contains image ocids. ", required=True)
+	parser.add_argument('--regions', nargs='+',
+						help="Updates the volume and backups tags from the given instance id", required=True)
+	parser.add_argument('--compartment_id', help='Provide the compartment ID where the images exist. (OPTIONAL) If not provided takes default compartment id')
+	parser.add_argument('--bucket_name', help="Provide bucket name for the images to be stored. OPTIONAL if not provided takes default bucket")
+	
+	
+	args = parser.parse_args()
+	PROFILE = args.profile
+	compartment_id = None
+	bucket_name = None
+	if(args.compartment_id):
+		compartment_id = args.compartment_id
+	if(args.bucket_name):
+		bucket_name = args.bucket_name
+	image_file = args.file
+	regions_list = args.regions
 	regions = list()
 	for j in regions_list:
-        region_destination = REGIONS_SHORT_NAMES[j]
-        regions.append(region_destination)
+		region_destination = REGIONS_SHORT_NAMES[j]
+		regions.append(region_destination)
 	m = Migrate(PROFILE, image_file, regions, compartment_id, bucket_name)
