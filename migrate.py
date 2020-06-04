bf3c6d9d4d96c27c3735f77018b1eca50c072106
csh-iaas-cec <ravi.devarakonda@oracle.com>
2020-06-05 00:41:09 +0530

    added append file fix

--- migrate.py
+++ migrate.py
@@ -228,7 +228,7 @@ class Migrate:
 			display_name=object_name,
 		)
 		image_details = cid.create_image(create_image_details=image_details).data
-		with open("images_details.txt","w") as f:
+		with open("images_details.txt","a") as f:
 			f.write(image_details.id+","+region+"\n")
 		print(f"Importing image {object_name} started successfully ")
 		logger.info(f"Importing image {object_name} started successfully ")
